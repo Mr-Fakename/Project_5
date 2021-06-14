e0ddecbabab7f3349484dc04582f9ddd834344f6
Mr-Fakename <58395707+Mr-Fakename@users.noreply.github.com>
2021-06-14 10:17:52 +0200

    Update api_calls.py

--- models/api_calls.py
+++ models/api_calls.py
@@ -1,24 +1,41 @@
 import requests
-import json
 
 
-url = "https://fr.openfoodfacts.org/cgi/search.pl?"
-headers = {"User-Agent": "PurBeurre/OpenClassRooms - Python/Windows - Version 1.0"}
-payload = {"search_simple": 1,
-           "action": "process",
-           "tagtype_0": "countries",
-           "tag_contains_0": "contains",
-           "tag_0": "france",
-           "sort_by": "unique_scans_n",
-           "page_size": 1000,
-           "json": 1,
-           "fields": "brands,url,stores,nutriscore_grade,"
-                     "categories,product_name_fr,code"
-           }
+class API:
+    def __init__(self):
+        self.raw_data = None
+        self.cleaned_data = []
 
-r = requests.get(url, params=payload, headers=headers)
+    def get_data(self):
+        url = "https://fr.openfoodfacts.org/cgi/search.pl?"
+        headers = {"User-Agent": "PurBeurre/OpenClassRooms - Python/Windows - Version 1.0"}
+        payload = {"search_simple": 1,
+                   "action": "process",
+                   "tagtype_0": "countries",
+                   "tag_contains_0": "contains",
+                   "tag_0": "france",
+                   "sort_by": "unique_scans_n",
+                   "page_size": 1000,
+                   "json": 1,
+                   "fields": "brands,url,stores,nutriscore_grade,"
+                             "categories,product_name_fr,code"
+                   }
 
-r_json = r.json()
+        r = requests.get(url, params=payload, headers=headers)
+        self.raw_data = r.json()
 
-with open('API_data.json', 'w') as f:
-    json.dump(r_json, f, indent=4)
+    def cleaner(self):
+        """ Takes json data that was extracted from the OpenFoodFacts API with the 'get_data()' function
+            Dict comprehension checks if some values are missing. If it is the case, the associated key
+            is deleted.
+            The function then loops over all products and stores those containing all the keys we want to use,
+            ensuring we only work with complete data.
+        """
+
+        self.raw_data = [{key: value for key, value in product.items() if value} for product in self.raw_data["products"]]
+
+        tags = ['brands', 'categories', 'code', 'nutriscore_grade', 'product_name_fr', 'stores', 'url']
+
+        for product in self.raw_data:
+            if all(key in product for key in tags):
+                self.cleaned_data.append(product)
