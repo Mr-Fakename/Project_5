c93fb75ec4600442505277b3fcb454aa661bced9
Mr-Fakename <58395707+Mr-Fakename@users.noreply.github.com>
2021-06-20 23:31:55 +0200

    Add colouring and text styling class

---
+++ views.py
@@ -0,0 +1,16 @@
+class Colors:
+
+    # TEXT STYLES
+    NORMAL = '\033[0m'
+    BOLD = '\033[1m'
+    ITALIC = '\033[3m'
+    UNDERLINE = '\033[4m'
+
+    # TEXT COLOURS
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
